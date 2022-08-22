4b656cc434708c54672aea9a694153d295580f43
SieR_VR <nwh63@naver.com>
2022-08-22 17:46:54 +0900

    module exports fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import ShaderProgram from "./ShaderProgram";
+
+export default ShaderProgram;
 export * from "./ShaderProgram";
 export * as GLTypes from "./glTypes";
 export * as GLFunctions from "./glFunctions";
